faa6d53f44d1c1b6fb7c87e6731f6e9e614baae0
Robert Habermann <mail@rhab.de>
2017-08-17 21:38:02 +0200

    remove dr-backup

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -16,7 +16,8 @@ from fabric.api import *
 # call with e.g. "fab -R prod df" for "prod" role host
 env.roledefs = {
     'self': ['localhost'],
-    'prod': ['dom0', 'dom1', 'dom5', 'dom1v2', 'dr-backup'],
+    #'prod': ['dom0', 'dom1', 'dom5', 'dom1v2', 'dr-backup'],
+    'prod': ['dom0', 'dom1', 'dom5', 'dom1v2'],
     'dev': ['user@dev.example.com'],
     'staging': ['user@staging.example.com'],
     'production': ['user@production.example.com']
