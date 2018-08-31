5e8486538c2754474ddaebe9de7101110ba7681a
Robert Habermann <mail@rhab.de>
2018-08-31 22:18:45 +0000

    update speedtest

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -2,5 +2,5 @@ alias ll='ls -la'
 alias lh='ls -lah'
 alias lr='ls -lahrS'
 alias ...='cd ../..'
-alias speedtest='wget --report-speed=bits -O /dev/null http://ftp.informatik.uni-frankfurt.de/debian.org/debian-cd/current/amd64/iso-cd/debian-8.3.0-amd64-netinst.iso'
+alias speedtest='wget --report-speed=bits -O /dev/null http://ftp.informatik.uni-frankfurt.de/debian.org/debian-cd/current/arm64/iso-cd/debian-9.5.0-arm64-netinst.iso'
 alias pip_upgrade_all="pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
