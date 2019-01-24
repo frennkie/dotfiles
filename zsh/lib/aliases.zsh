d935f22cbddd2c934e03d06309aa5d024e13afb0
Robert Habermann <mail@rhab.de>
2019-01-24 13:21:18 +0100

    add dux

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -4,3 +4,9 @@ alias lr='ls -lahrS'
 alias ...='cd ../..'
 alias speedtest='wget --report-speed=bits -O /dev/null http://ftp.informatik.uni-frankfurt.de/debian.org/debian-cd/current/arm64/iso-cd/debian-9.5.0-arm64-netinst.iso'
 alias pip_upgrade_all="pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
+
+dux() {
+    echo "checking disk usage in: $1"
+    du --max-depth=1 -x -BM $1 | sort -nr
+}
+
